a73af5f8b236636bdd3818f04063fe84f4d128eb
Mirraz Ibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-10-17 11:24:42 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118056/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118056.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ */
+public class Petugas {
+    
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+}
